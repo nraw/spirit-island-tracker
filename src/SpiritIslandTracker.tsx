e93b06b33327b0e6d3802a906f5038865899805a
Andrej-Marsic <andrej_marsic@mckinsey.com>
2025-03-11 21:20:10 +0100

    fix: I have no idea what went wrong

--- src/SpiritIslandTracker.tsx
+++ src/SpiritIslandTracker.tsx
@@ -549,7 +549,7 @@ const SpiritIslandTracker = () => {
 										<img
 											src={spirit.image_url}
 											alt={spirit.spirit}
-											className="mr-4 w-8 h-8 lg:w-16 lg:h-16 rounded-full object-cover object-center shadow-md"
+											className="mr-4 w-16 h-16 rounded-full object-cover object-center shadow-md"
 										/>
 										<span className="font-medium text-gray-200">
 											{spirit.spirit}
